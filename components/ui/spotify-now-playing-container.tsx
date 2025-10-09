9912b21297200dce91789c1ac3f16b163bbd3afa
ArazMaher <Araz.maher.88@gmail.com>
2025-10-10 00:10:33 +0330

    debug image url in hero.tsx

--- components/ui/spotify-now-playing-container.tsx
+++ components/ui/spotify-now-playing-container.tsx
@@ -5,7 +5,7 @@ export function SpotifyNowPlayingContainer() {
   const track = {
     title: "Tomhet",
     artist: "Burzum",
-    albumArtUrl: "./images/tomhet.jpg",
+    albumArtUrl: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/tomhet.jpg`,
     songUrl: "https://www.youtube.com/watch?v=aTtNe8E0AoQ&list=RDaTtNe8E0AoQ&start_radio=1",
   };
 
